855f735434e06f3a3a496c4f58addf16debc9058
vtion001 <generalmanager.tiongsons@gmail.com>
2026-01-13 13:50:26 +0800

    fix sql issue on deployment

--- public/index.php
+++ public/index.php
@@ -8,6 +8,11 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+// Emergency Cache Clear for Deployment Fix
+if (file_exists(__DIR__.'/../bootstrap/cache/config.php')) {
+    unlink(__DIR__.'/../bootstrap/cache/config.php');
+}
+
 require __DIR__.'/../vendor/autoload.php';
 
 (require_once __DIR__.'/../bootstrap/app.php')
